1d838f1042c960cc33b0a13d172eaca978e7b111
Joshua Laoli <130970787+joshualaoli@users.noreply.github.com>
2023-07-12 09:45:27 +0700

    1/100 Java Script challenge

---
+++ Day 1/index.js
@@ -0,0 +1,30 @@
+new TradingView.widget ( {
+    "autosize" : true,
+    "symbol" : "BINANCE:BTCUSDT",
+    "interval" : "240",
+    "timezone" : "Etc/Utc",
+    "theme" : "dark",
+    "style" : "1",
+    "locale" : "en",
+    "toolbar-bg" : "#f1f3f6",
+    "enable-publisihing" : false,
+    "withdaterangers" : true,
+    "hide_side_toolbar" : false,
+    "allow_symbol_change" : true,
+    "watchlist" : [
+        "BINANCE: BTCUSDT",
+        "BINANCE: ETHUSDT"
+    ],
+    "details" : true,
+    "hotlist" : true,
+    "calender" : true,
+    "studies" : [
+        "STD;SMA"
+    ],
+    "container_id" : "chart",
+    "show_popup_button" : true,
+    "popup_width" : "1000",
+    "pupup_height" : "650",
+
+
+})
